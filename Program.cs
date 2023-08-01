1052fd89464ad24caadff9a7d44bc029b30c39d8
skafula <79100340+skafula@users.noreply.github.com>
2023-08-01 15:24:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<List<int>> ints = new List<List<int>>();
+        ints.Add(new List<int>() { 3, 5, 8 });
+        ints.Add(new List<int>() { 11, 30, 20, 5, 10 });
+        ints.Add(new List<int>() { 330, 22, 213 });
+
+        foreach (int highest in FindLargest(ints))
+        {
+            Console.WriteLine(highest);
+        }
+    }
+        public static List<int> FindLargest(List<List<int>> listInts)
+        {
+            List<int> result = new List<int>();
+
+            foreach (List<int> ints in listInts)
+            {
+                ints.Sort();
+                result.Add(ints[ints.Count - 1]);
+            }
+            return result;
+        }
+}
